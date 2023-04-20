fd52425fba1c8b83f172505eb8371cd5750ba1d5
Francesco <fbaldini02@gmail.com>
2023-04-20 17:48:34 +0200

    Generated Combined Maps

--- noise_gen/combine_edges.js
+++ noise_gen/combine_edges.js
@@ -8,19 +8,15 @@ const canvas = createCanvas(width, height);
 const ctx = canvas.getContext('2d');
 const imgData = ctx.createImageData(width, height);
 
-const mapImg = new Image();
-mapImg.src = './rawMaps/raw1.png';
-mapImg.onload = () => {
+loadImage('./rawMaps/raw1.png').then((mapImg) => {
 
     ctx.drawImage(mapImg, 0, 0, width, height);
     const imageData = ctx.getImageData(0, 0, width, height);
 
-    const mapEdges = new Image();
-    mapEdges.src = './edges.png';
-    mapEdges.onload = () => {
+    loadImage('./edges.png').then((mapEdges) => {
 
         ctx.clearRect(0, 0, width, height);
-        ctx.drawImage(mapImg, 0, 0, width, height);
+        ctx.drawImage(mapEdges, 0, 0, width, height);
         const edgesData = ctx.getImageData(0, 0, width, height);
 
         for (let x = 0; x < width; x++) {
@@ -33,9 +29,9 @@ mapImg.onload = () => {
                 const re = Math.floor(edgesData.data[i]);
                 const ge = Math.floor(edgesData.data[i + 1]);
                 const be = Math.floor(edgesData.data[i + 2]);
-                imgData.data[i] = re === 0 || re === 255 ? re : rm;
-                imgData.data[i + 1] = ge === 0 || ge === 255 ? ge : gm;
-                imgData.data[i + 2] = be === 0 || be === 255 ? be : bm;
+                imgData.data[i] = re < 10 || re > 245 ? re : rm;
+                imgData.data[i + 1] = ge < 10 || ge > 245 ? ge : gm;
+                imgData.data[i + 2] = be < 10 || be > 245 ? be : bm;
                 imgData.data[i + 3] = 255;
             }
         }
@@ -43,7 +39,7 @@ mapImg.onload = () => {
         ctx.putImageData(imgData, 0, 0);
 
         const buffer = canvas.toBuffer('image/png');
-        fs.writeFileSync('../public/noiseMaps/map.png', buffer);
-    }
+        fs.writeFileSync('../public/noiseMaps/map1.png', buffer);
+    });
 
-};
+});
